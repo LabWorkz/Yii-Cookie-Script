879eeeba75b31830773bf831fcd1d02e33926739
LabWorkz <gary@labworkz.com>
2014-02-05 16:53:11 -0600

    Add Script

---
+++ CookieCreate.php
@@ -0,0 +1,11 @@
+<?php
+
+Yii::app()->Cookies->putCMsg('plans',
+serialize(array( '0' => array('id' => 1, 'title' => 'Service 1', 'min_price' => '0.00', 'max_price' => '100.00')),
+array( '1' => array('id' => 2, 'title' => 'Service 2', 'min_price' => '0.00', 'max_price' => '300.00')),
+array( '2' => array('id' => 3, 'title' => 'Service 3', 'min_price' => '0.00', 'max_price' => '200.00')),
+array( '3' => array('id' => 4, 'title' => 'Service 4', 'min_price' => '0.00', 'max_price' => '500.00'))
+),
+array('expire' => time() + 3600)
+);
+
